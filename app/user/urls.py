a4254a65b33874ab233eff77083e6c0a907c7ceb
Christian <cackren@gmail.com>
2020-08-22 00:39:47 +0300

    Added user/profile/ path to app/user/urls.py

--- app/user/urls.py
+++ app/user/urls.py
@@ -7,5 +7,6 @@ app_name = 'user'
 
 urlpatterns = [
     path('create/', views.CreateUserView.as_view(), name='create'),
+    path('profile/', views.ManageUserView.as_view(), name='profile'),
     path('token/', views.CreateTokenView.as_view(), name='token'),
 ]
