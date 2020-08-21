5f5efa94f75ad4831bb259b0dec1965765513577
Christian <cackren@gmail.com>
2020-08-21 21:38:19 +0300

    Corrected typo in test_user_api.py

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -29,7 +29,7 @@ class PublicUserApiTests(TestCase):
         res = self.client.post(CREATE_USER_URL, payload)
 
         self.assertEqual(res.status_code, status.HTTP_201_CREATED)
-        user = get_user_model().objects.get(**res)
+        user = get_user_model().objects.get(**res.data)
         self.assertTrue(user.check_password(payload['password']))
         self.assertNotIn('password', res.data)
 
