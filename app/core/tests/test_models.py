656213e93d020226fdd1bd9967b8485bbbabe245
Christian <cackren@gmail.com>
2020-08-21 15:08:51 +0300

    Test for checking empty email response

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -5,7 +5,7 @@ from django.contrib.auth import get_user_model
 class ModelTests(TestCase):
 
     def test_create_user_with_email_successful(self):
-        '''Test if creating a new user with an email is successful'''
+        '''Test: creating a new user with an email is successful'''
         email = 'example@example.com'
         password = 'django123!'
         user = get_user_model().objects.create_user(
@@ -17,8 +17,13 @@ class ModelTests(TestCase):
         self.assertTrue(user.check_password(password))
 
     def test_new_user_email_normalized(self):
-        '''Test if the email for a new user is normalized'''
+        '''Test: the email for a new user is normalized'''
         email = 'example@Example.Com'
         user = get_user_model().objects.create_user(email, 'django123!')
 
         self.assertEqual(user.email, email.lower())
+
+    def test_new_user_invalid_email(self):
+        '''Test: creating user with no email raises error'''
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(None, 'django123!')
