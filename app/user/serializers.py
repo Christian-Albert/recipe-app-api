9b6fef1b329a643d135bb6e5ad4f4aa5bd7ea022
Christian <cackren@gmail.com>
2020-08-21 23:40:15 +0300

    Corrected typo in password style attribute

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -21,7 +21,7 @@ class AuthTokenSerializer(serializers.Serializer):
     '''Serializer for the user object authentication object'''
     email = serializers.CharField()
     password = serializers.CharField(
-        style={'input-type': 'password'},
+        style={'input_type': 'password'},
         trim_whitespace=False
     )
 
