9ef69af25b1c67e76ccffe09334ee6889fcc5aae
Christian <cackren@gmail.com>
2020-08-22 22:03:27 +0300

    Created create recipe endpoint

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -10,7 +10,11 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = get_user_model()
         fields = ('email', 'password', 'name')
-        extra_kwargs = {'password': {'write_only': True, 'min_length': 5}}
+        extra_kwargs = {'password': {
+            'write_only': True, 'min_length': 5, 'style': {
+                'input_type': 'password'
+            }
+            }}
 
     def create(self, validated_data):
         '''Create a new user with encrypted password and return user'''
